0148bc1f10f2f2ca85fc15cea83f860202158a94
Miguel Ramos López <miguelon2001b@gmail.com>
2021-06-30 11:26:21 +0200

    mejoras Bsearch y Tsearch

--- DyV/src/BinarySearch.java
+++ DyV/src/BinarySearch.java
@@ -5,17 +5,15 @@ public class BinarySearch {
         for (int i = 0; i < a.length; i++ )
             System.out.print(a[i] + " ");
         System.out.println();
-        System.out.println(binarysearch(a, 0, a.length-1, 5));
+        System.out.println("Posicion del numero dentro del array: (0-8) " + binarysearch(a, 0, a.length-1, 8));
         
     }
 
     public static int binarysearch(int[] arr, int i, int f, int num) {
         if (num == arr[i]) {
-            System.out.println("El numero " + num + " se encuentra en la primera pos");
             return i;
         } 
         if (num == arr[f]) {
-            System.out.println("El numero " + num + " se encuentra en la ultima pos");
             return f;
         } else {
             int h = (f + i) / 2;
@@ -25,7 +23,6 @@ public class BinarySearch {
             if (num > arr[h]) {
                 return binarysearch(arr, h+1, f, num);
             } else {
-                System.out.println("El numero " + num + " se encuentra en la pos central (h)");
                 return h;
             }
 
---
+++ DyV/src/TernarySearch.java
@@ -0,0 +1,35 @@
+public class TernarySearch {
+
+    
+    public static void main(String[] args) throws Exception {
+        int[] a = {1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i < a.length; i++ )
+            System.out.print(a[i] + " ");
+        System.out.println();
+        System.out.println("Posicion del numero dentro del array: (0-8) " +ternarysearch(a, 0, a.length-1, 7));
+        
+    }
+
+    public static int ternarysearch(int[] arr, int i, int f, int num) {
+        if (num == arr[i]) {
+            return i;
+        } 
+        if (num == arr[f]) {
+            return f;
+        } else {
+            int h_1 = (2 * i + f) / 3;
+            int h_2 = (i + 2 * f) / 3;
+
+            if (num < arr[h_1]) {
+                return ternarysearch(arr, i, h_1, num);
+            }
+            if (num > arr[h_2]) {
+                return ternarysearch(arr, h_2 + 1, f, num);
+            } else {
+                return ternarysearch(arr,h_1 + 1, h_2, num);
+            }
+
+        }
+    }
+    
+}
