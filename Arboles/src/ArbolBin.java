4f722eeb89b0556a4945f4fed618ce28c1a10c3f
Miguel Ramos López <miguelon2001b@gmail.com>
2021-07-04 11:49:22 +0200

    arbolBin y teoria

--- Arboles/src/ArbolBin.java
+++ Arboles/src/ArbolBin.java
@@ -1,6 +1,7 @@
 public class ArbolBin<T> {
 
 	class NodoBin {
+
 		T info;
 		NodoBin hijoIzq;
 		NodoBin hijoDcho;
@@ -56,20 +57,87 @@ public class ArbolBin<T> {
 			this.hijoDerecho().dibujar(nivel+1);			
 		}
 	}
+
+	public void preorden() {
+		if (!this.esVacio()) {
+			System.out.print(this.raiz() + " -");
+			if (!this.hijoIzquierdo().esVacio()) {
+				this.hijoIzquierdo().preorden();
+			}
+			if (!this.hijoDerecho().esVacio()) {
+				this.hijoDerecho().preorden();
+			}
+		}
+	}
+
+
+	public void postorden() {
+		if (!this.esVacio()) {
+			if (!this.hijoIzquierdo().esVacio()) {
+				this.hijoIzquierdo().postorden();
+			}
+			if (!this.hijoDerecho().esVacio()) {
+				this.hijoDerecho().postorden();
+			}
+			System.out.print(this.raiz() + " -");
+		}
+	}
+
+	public void inorden() {
+		if (!this.esVacio()) {
+			if (!this.hijoIzquierdo().esVacio()) {
+				this.hijoIzquierdo().inorden();
+			}
+			System.out.print(this.raiz() + " -");
+			if (!this.hijoDerecho().esVacio()) {
+				this.hijoDerecho().inorden();
+			}
+		}
+	}
+
 		
 
 	public static void main(String[] args) {
-		//ArbolBin<Integer> g = new ArbolBin<Integer>(new ArbolBin<Character>(),'G',new ArbolBin<Character>());
-		ArbolBin<Integer> d = new ArbolBin<Integer>(new ArbolBin<Integer>(),4,new ArbolBin<Integer>());
-		ArbolBin<Integer> f = new ArbolBin<Integer>(new ArbolBin<Integer>(),6,new ArbolBin<Integer>());
-		//ArbolBin<Character> e = new ArbolBin<Character>(new ArbolBin<Character>(),'E',f);
-		ArbolBin<Integer> e = new ArbolBin<Integer>(new ArbolBin<Integer>(),5,f);
-		ArbolBin<Integer> b = new ArbolBin<Integer>(d,2,new ArbolBin<Integer>());
-		ArbolBin<Integer> c = new ArbolBin<Integer>(e,3,new ArbolBin<Integer>());
+		/* Para instanciar un nodo, lo que hacemos es crear un objeto de la clase ArbolBin, del tipo concreto que queramos ya que esta inicializado como <T>*/
 		
-		ArbolBin<Integer> a = new ArbolBin<Integer>(b,1,c);
+		/* Instanciar nivel 4*/
+		ArbolBin<Integer> hijoDer_3 = new ArbolBin<Integer>(new ArbolBin<Integer>(),7,new ArbolBin<Integer>());
 
-		a.dibujar(1);
+		/*Instanciar nivel 3*/
+		ArbolBin<Integer> hijoDer_2 = new ArbolBin<Integer>(new ArbolBin<Integer>(),5,hijoDer_3);
+		ArbolBin<Integer> hijoIzq_2 = new ArbolBin<Integer>(new ArbolBin<Integer>(),6,new ArbolBin<Integer>());
+		ArbolBin<Integer> hijoIzq_3 = new ArbolBin<Integer>(new ArbolBin<Integer>(),4,new ArbolBin<Integer>());
+		
+		
+		/* Intanciar nivel 2*/
+		ArbolBin<Integer> hijoDer_1 = new ArbolBin<Integer>(hijoIzq_2,3,new ArbolBin<Integer>());
+		ArbolBin<Integer> hijoIzq_1 = new ArbolBin<Integer>(hijoIzq_3,2,hijoDer_2);
+		
+		/* Instanciar raiz (nivel 1)*/
+		ArbolBin<Integer> raiz = new ArbolBin<Integer>(hijoIzq_1,1,hijoDer_1);
+
+		/* dibujamos el arbol*/
+		raiz.dibujar(1);
+
+		/* recorremos el arbol*/
+		System.out.println("Recorrido preorden: ");
+		raiz.preorden();
+		System.out.println();
+
+		System.out.println("Recorrido postorden: ");
+		raiz.postorden();
+		System.out.println();
+
+		System.out.println("Recorrido inorden: ");
+		raiz.inorden();
+		System.out.println();
+
+
+
+
+		/* También pueden instanciarse de otros tipos como de caracteres: */
+		ArbolBin<Character> g = new ArbolBin<Character>(new ArbolBin<Character>(),'G',new ArbolBin<Character>());
+		ArbolBin<Character> h = new ArbolBin<Character>(new ArbolBin<Character>(),'H',g);
 	
 
 
