4efb7fd4c6bd7e20a42eb9d3cc974d0efe699a36
Miguel Ramos López <miguelon2001b@gmail.com>
2021-07-07 16:43:40 +0200

    Algoritmo mochila completo

--- Avance_Rapido/src/Mochila.java
+++ Avance_Rapido/src/Mochila.java
@@ -9,33 +9,50 @@ public class Mochila {
         int [] pesos = {10, 3, 3, 4};
 
         /* array de benficios para cada peso posible */
-        int [] beneficios = {10, 3, 3, 4};
+        int [] beneficios = {10, 9, 9, 9};
 
-        int solucion[] = new int [pesos.length] ;
+        int solucion[] = new int [pesos.length];
 
         mochila(pesos, beneficios, m, solucion);
+
+        //int [] beneficios2 = {10, 1, 1, 1};
+
+        //int solucion2[] = new int [pesos.length];
+
+        //mochila(pesos, beneficios2, m, solucion2);
 
     }
 
     public static void mochila (int [] pesos, int [] beneficios, int m, int[] solucion) {
 
-        for (int i = 0; i < pesos.length; i++) {
+        /* pesos y beneficios tienen que estar ordenados decrecientemente*/
+
+        /* recorremos desde 1 hasta n*/
+        for (int i = 1; i < pesos.length; i++) {
+            /*  inciando todo a ceros*/
             solucion[i] = 0;
         }
 
+        /* para calibrar la capacidad que va teniendo la mochila a medida que vamos iterando*/
         int capacidad = m;
         int i = 1;
 
+        /* mientras que no sobrepasemos la capacidad y no recorramos todo el array de pesos*/
         while (i < pesos.length && pesos[i] <= capacidad) {
+            /* actualizamos la capacidad restando el peso que ya hemos obtenido */
             capacidad = capacidad - pesos[i];
+            /* en los valores que vayan cumpliendo ponemos un 1, es decir, los que sena optimos usar*/
             solucion[i] = 1;
             i++;
         }
 
-        if (i <= pesos.length && m < capacidad) {
+        /* creamos la sol más optima, siendo el planteamiento C (ver imagen de solucion explicada)*/
+        if (i < pesos.length) {
+            /* metemos en solu los pesos que hemos pillado para la capacidad de la mochila*/
             solucion[i] = capacidad / pesos[i];
         }
 
+        /* imprimimos la solu*/
         for (int j = 0; j < solucion.length; j++) {
             System.out.print(solucion[j] + " ");
         }
